517768415f2f0dcf5860d7ec24b3184258652252
versroot <131954932+versroot@users.noreply.github.com>
2024-08-05 22:21:58 +0200

    gallery and form

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,6 @@ export default defineConfig(({ command }) => {
     root: 'src',
     build: {
       sourcemap: true,
-
       rollupOptions: {
         input: glob.sync('./src/*.html'),
         output: {
@@ -26,5 +25,8 @@ export default defineConfig(({ command }) => {
       outDir: '../dist',
     },
     plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    optimizeDeps: {
+      include: ['simplelightbox'],
+    },
   };
 });
